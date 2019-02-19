e85d711a2aefc043b5b46e210603c614662b697d
Karine Valenca <valenca.karine@gmail.com>
2019-02-18 23:50:08 -0300

    add logic to select flavours acording box size

--- candyBox.js
+++ candyBox.js
@@ -7,6 +7,11 @@ var step2 = document.querySelector(".step2");
 var step3 = document.querySelector(".step3");
 var step4 = document.querySelector(".step4");
 var step5 = document.querySelector(".step5");
+var step6 = document.querySelector(".step6");
+var boxSizeText = document.querySelector("#boxSize");
+var flavours = document.querySelector("#flavours");
+var boxSize = "";
+var submit = document.querySelector("#submit");
 
 for(var i = 0; i < candyButtons.length; i++) {
 	candyButtons[i].addEventListener("click", function(){
@@ -17,17 +22,14 @@ for(var i = 0; i < candyButtons.length; i++) {
 
 for(var i = 0; i < sizeButtons.length; i++) {
 	sizeButtons[i].addEventListener("click", function(){
+		boxSize = this.textContent;
+		boxSizeText.textContent = boxSize;
 		step2.style.display = "none";
 		step3.style.display = "block";
+		selectCandyFlavours(boxSize);
 	});
 }
 
-for(var i = 0; i < flavourButtons.length; i++) {
-	flavourButtons[i].addEventListener("click", function(){
-		step3.style.display = "none";
-		step4.style.display = "block";
-	});
-}
 
 for(var i = 0; i < boxStyleButtons.length; i++) {
 	boxStyleButtons[i].addEventListener("click", function(){
@@ -35,3 +37,22 @@ for(var i = 0; i < boxStyleButtons.length; i++) {
 		step5.style.display = "block";
 	});
 }
+
+function selectCandyFlavours(quantity){
+	for(var i = 0; i < flavourButtons.length; i++) {
+		var count = 0;
+		flavourButtons[i].addEventListener("click", function(){
+			count += 1;
+			flavours.textContent += " " + this.textContent + " ";
+			if (count >= quantity){
+				step3.style.display = "none";
+				step4.style.display = "block";
+			}
+		});
+	}
+}
+
+submit.addEventListener("click", function(){
+	step5.style.display = "none";
+	step6.style.display = "block";
+})
